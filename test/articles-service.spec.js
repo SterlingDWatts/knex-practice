ea2f66bb8c522263f2baef22b75badda9fd5c2da
Sterling Watts <SterlingDWatts@gmail.com>
2020-02-01 16:22:28 -0800

    Temp test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,6 +1,16 @@
 const ArticleService = require("../src/articles-service");
+const knex = require("knex");
 
 describe(`getAllArticles()`, () => {
+  let db;
+
+  before(() => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DB_URL
+    });
+  });
+
   it(`resolves all articles from 'blogful_articles' table`, () => {
     // test that ArticlesService.getAllArticles gets data from the table
   });
